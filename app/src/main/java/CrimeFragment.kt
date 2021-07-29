dee58990d1daa06959e78f5d8f33b208cc6fb610
sasha <sasadanilina2@gmail.com>
2021-07-29 18:40:26 +0500

    2 day

---
+++ app/src/main/java/CrimeFragment.kt
@@ -0,0 +1,27 @@
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import com.bignerdranch.android.criminalintent.Crime
+import com.bignerdranch.android.criminalintent.R
+
+class CrimeFragment : Fragment() {
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        return view
+    }
+}
