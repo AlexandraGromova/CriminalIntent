f288edf784b21dc37f9c2b31168360108c91f7c0
sasha <sasadanilina2@gmail.com>
2021-08-02 19:16:45 +0500

    3 day

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent
 
+import CrimeFragment
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null){
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
