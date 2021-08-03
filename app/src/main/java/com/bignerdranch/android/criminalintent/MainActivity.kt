2a98d1c07bd05c07529a6ed03b225a849710ff22
sasha <sasadanilina2@gmail.com>
2021-08-03 14:15:27 +0500

    Re index done

--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.bignerdranch.android.criminalintent
 
-import CrimeFragment
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bignerdranch.CrimeListFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-        if (currentFragment == null){
-            val fragment = CrimeFragment()
+        if (currentFragment == null) {
+            val fragment = CrimeListFragment.newInstance()
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.fragment_container, fragment)
