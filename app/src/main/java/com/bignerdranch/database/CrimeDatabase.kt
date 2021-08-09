e823764626ea5ccf8b45ab9a23f13bbbf88aa199
sasha <sasadanilina2@gmail.com>
2021-08-09 19:12:32 +0500

    начала делать базу данных Room Library.

---
+++ app/src/main/java/com/bignerdranch/database/CrimeDatabase.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.bignerdranch.android.criminalintent.Crime
+
+@Database(entities = [ Crime ::class], version = 1)
+@TypeConverters(TypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+
+}
