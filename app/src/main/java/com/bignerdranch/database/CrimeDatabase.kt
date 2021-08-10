2385d18977d70a18e3e97ebacc2d0bc5c8e0064e
sasha <sasadanilina2@gmail.com>
2021-08-10 17:45:04 +0500

    начала делать репозиторий. Узнала что такое Singleton(шаблон)

--- app/src/main/java/com/bignerdranch/database/CrimeDatabase.kt
+++ app/src/main/java/com/bignerdranch/database/CrimeDatabase.kt
@@ -8,5 +8,5 @@ import com.bignerdranch.android.criminalintent.Crime
 @Database(entities = [ Crime ::class], version = 1)
 @TypeConverters(TypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
-
+ abstract fun crimeDao(): CrimeDao
 }
